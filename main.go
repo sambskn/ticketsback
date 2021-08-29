65fcc429f36a447b694501c9e7fc1f5ef0cf5e74
sambskn <sambaskin@ou.edu>
2021-08-29 17:53:27 -0500

    adding sample homepage -sb

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/websocket"
-
 	"flag"
+	"html/template"
 	"log"
+	"os"
+
+	"github.com/gorilla/websocket"
 )
 
 // type album struct {
@@ -23,7 +25,7 @@ import (
 // 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 // }
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", os.Getenv("ADDR"), "http service address")
 
 var upgrader = websocket.Upgrader{} // use defaults lol
 
@@ -50,10 +52,16 @@ func ticketDispenser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/ticket")
+}
+
 func main() {
+	log.Print("bootin up")
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/ticket", ticketDispenser)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 
 	// router := gin.Default()
@@ -63,6 +71,80 @@ func main() {
 	// router.Run("localhost:8080")
 
 }
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td></tr></table>
+</body>
+</html>
+`))
 
 // func getAlbums(c *gin.Context) {
 // 	c.IndentedJSON(http.StatusOK, albums)
